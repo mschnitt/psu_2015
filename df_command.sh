19a471b4ad40d646f1e713bd1aeb5a4c1d7ffee7
mschnitt <mschnitt@yahoo.com>
2015-07-04 11:20:01 -0700

    Adding files for course content

---
+++ df_command.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+open /Applications
+
+open /Applications/Utilities/Console.app
+
+open http://www.disneyanimation.com
+
+open -a safari http://www.google.com
+
+open /Library/Desktop\ Pictures/Frog.jpg
+
+open /Library/Desktop\ Pictures/Yosemite.jpg
---
+++ open_command.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+open /Applications
+
+open /usr/bin
+
+open /Applications/Utilities/Console.app
+
+open http://www.disneyanimation.com
+
+open /Library/Desktop\ Pictures/Frog.jpg
+
+open “/Library/Desktop Pictures/Yosemite.jpg”
+
+open -a safari http://www.google.com
+
+open -a google chrome http://disneycareers.com/en/default/
