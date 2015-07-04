5597480edc7efbf119ce63462747e950c49add61
mschnitt <mschnitt@yahoo.com>
2015-07-04 14:27:21 -0700

    Updates to supporting files

--- diskutil_command.sh
+++ diskutil_command.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 
-diskutil list
-
-diskutil info /dev/disk0
-
-diskutil info /dev/disk0s1
-
-diskutil
--- du_df_commands.sh
+++ du_df_commands.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
 
-open /Applications
-
-open /Applications/Utilities/Console.app
-
-open http://www.disneyanimation.com
-
-open -a safari http://www.google.com
-
-open /Library/Desktop\ Pictures/Frog.jpg
-
-open /Library/Desktop\ Pictures/Yosemite.jpg
